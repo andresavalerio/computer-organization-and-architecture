5ab44206c0ea6ac0ce60e9a8157190cf84038f80
andresavalerio <andresa.valerio@unifesp.br>
2023-09-14 16:31:37 -0300

    fix: linting

--- intro_to_mips/integer_double.asm
+++ intro_to_mips/integer_double.asm
@@ -6,7 +6,7 @@
 # If number greater than 0, print integer double
 #########################################################
 
-	.data
+    .data
 msg1: .asciiz "Enter integer: "
 fnl: .asciiz "\nFinishing script.\n"
 
@@ -15,30 +15,30 @@ fnl: .asciiz "\nFinishing script.\n"
 
 main:
     # printing message to user
-    li	$v0,4
-    la	$a0, msg1
+    li $v0,4
+    la $a0, msg1
     syscall
 
     # recieving user input
-	li	$v0,5
-	syscall	
-	move	$t0,$v0 # moving input to register
-	
-	# if clause
+    li $v0,5
+    syscall
+    move $t0,$v0 # moving input to register
+
+    # if clause
     blez $t0, exit
-	add $t0, $t0, $t0 #calculating double
-	
-	# printing integer
-	li $v0, 1
+    add $t0, $t0, $t0 #calculating double
+
+    # printing integer
+    li $v0, 1
     move $a0, $t0
     syscall
 
 exit:
     # printing message to user
-    li	$v0,4
-    la	$a0, fnl
+    li $v0,4
+    la $a0, fnl
     syscall
 
     # ending script
     li	$v0,10
-	syscall
+    syscall
